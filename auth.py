91ef70e8401e77b00a2f758485e3d4458b9f2e1c
Dhruv Jain <dhruv_jains@hotmail.com>
2019-02-16 22:39:38 -0500

    Git check and cloning of public repositories

---
+++ auth.py
@@ -0,0 +1,32 @@
+import subprocess
+import os
+import sys
+
+
+log = open("log.txt", "w")
+error = open ("error.txt", "w")
+
+
+def check_git():
+    try:
+        subprocess.check_call(["git", "-version"], stdout=log, stderr=error)
+    except subprocess.CalledProcessError:
+        print("Please install github")
+
+
+def clone_repo():
+    github_user = (input("Please enter your github username:"))
+    github_repo = (input("Please enter your github repo URL:"))
+    command = str("curl -u %s https://api.github.com/user" % github_user)
+    os.system(command)
+    command = str("git clone %s" % github_repo)
+    os.system(command)
+
+check_git()
+clone_repo()
+
+
+
+
+
+
